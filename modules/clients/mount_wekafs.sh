40e5967b0eaf232aac6204e6f1b829a7a67c391b
Kristina Solovyova <kristina.solovyova@weka.io>
2023-06-06 19:59:36 +0300

    feat: install weka on clients from backend

--- modules/clients/mount_wekafs.sh
+++ modules/clients/mount_wekafs.sh
@@ -1,9 +1,32 @@
+IFS=" " read -ra ips <<< "${backend_ips}"
+backend_ip="$${ips[RANDOM % $${#ips[@]}]}"
+# install weka using random backend ip from ips list
+function retry_weka_install {
+  retry_max=60
+  retry_sleep=10
+  count=$retry_max
+
+  while [ $count -gt 0 ]; do
+      curl --fail $backend_ip:14000/dist/v1/install > install_script.sh && break
+      count=$(($count - 1))
+      backend_ip="$${ips[RANDOM % $${#ips[@]}]}"
+      echo "Retrying weka install from $backend_ip in $retry_sleep seconds..."
+      sleep $retry_sleep
+  done
+  [ $count -eq 0 ] && {
+      echo "weka install failed after $retry_max attempts"
+      return 1
+  }
+  chmod +x install_script.sh && ./install_script.sh
+  return 0
+}
+
+retry_weka_install
+
 FILESYSTEM_NAME=default # replace with a different filesystem at need
 MOUNT_POINT=/mnt/weka # replace with a different mount point at need
 mkdir -p $MOUNT_POINT
 
-weka local stop
-weka local rm default --force
 weka local stop && weka local rm -f --all
 
 gateways="${all_gateways}"
@@ -11,37 +34,12 @@ subnets="${all_subnets}"
 NICS_NUM="${nics_num}"
 eth0=$(ifconfig | grep eth0 -C2 | grep 'inet ' | awk '{print $2}')
 
-mount_command="mount -t wekafs -o net=udp ${backend_ip}/$FILESYSTEM_NAME $MOUNT_POINT"
+mount_command="mount -t wekafs -o net=udp $backend_ip/$FILESYSTEM_NAME $MOUNT_POINT"
 if [[ ${mount_clients_dpdk} == true ]]; then
   getNetStrForDpdk $(($NICS_NUM-1)) $(($NICS_NUM)) "$gateways" "$subnets" "-o net="
-  mount_command="mount -t wekafs $net -o num_cores=1 -o mgmt_ip=$eth0 ${backend_ip}/$FILESYSTEM_NAME $MOUNT_POINT"
+  mount_command="mount -t wekafs $net -o num_cores=1 -o mgmt_ip=$eth0 $backend_ip/$FILESYSTEM_NAME $MOUNT_POINT"
 fi
 
-cat <<'EOF' > retry_mount.sh
-#!/bin/bash
-
-mount_command=$1
-retry_count=0
-max_retries=60
-retry_interval=30
-
-while true; do
-    eval "$mount_command" && break
-
-    ((retry_count++))
-    if [ $retry_count -eq $max_retries ]; then
-        echo "Mount wekafs failed after $max_retries retries."
-        exit 1
-    fi
-
-    echo "Mount wekafs failed. Retrying in $retry_interval seconds..."
-    sleep $retry_interval
-done
-
-echo "Mounted wekafs successfully!"
-EOF
-
-chmod +x retry_mount.sh
-./retry_mount.sh "$mount_command"
+retry 60 30 $mount_command
 
 rm -rf $INSTALLATION_PATH
--- preparation.sh
+++ preparation.sh
@@ -79,3 +79,73 @@ apt install -y jq
 
 # remove installation path before installing weka
 rm -rf $INSTALLATION_PATH
+
+# getNetStrForDpdk bash function definitiion
+function getNetStrForDpdk() {
+  i=$1
+  j=$2
+  gateways=$3
+  subnets=$4
+  net_option_name=$5
+
+  if [ "$#" -lt 5 ]; then
+      echo "'net_option_name' argument is not provided. Using default value: --net"
+      net_option_name="--net "
+  fi
+
+  if [ -n "$gateways" ]; then #azure and gcp
+    gateways=($gateways)
+  fi
+
+  if [ -n "$subnets" ]; then #azure only
+    subnets=($subnets)
+  fi
+
+  net=" "
+  for ((i; i<$j; i++)); do
+    if [ -n "$subnets" ]; then
+      subnet=$${subnets[$i]}
+      subnet_inet=$(curl -s -H Metadata:true –noproxy “*” http://169.254.169.254/metadata/instance/network\?api-version\=2021-02-01 | jq --arg subnet "$subnet" '.interface[] | select(.ipv4.subnet[0].address==$subnet)' | jq -r .ipv4.ipAddress[0].privateIpAddress)
+      eth=$(ifconfig | grep -B 1 "$subnet_inet" |  head -n 1 | cut -d ':' -f1)
+    else
+      eth=eth$i
+      subnet_inet=$(ifconfig $eth | grep 'inet ' | awk '{print $2}')
+    fi
+    if [ -z "$subnet_inet" ];then
+      net=""
+      break
+    fi
+    enp=$(ls -l /sys/class/net/$eth/ | grep lower | awk -F"_" '{print $2}' | awk '{print $1}') #for azure
+    if [ -z $enp ];then
+      enp=$(ethtool -i $eth | grep bus-info | awk '{print $2}') #pci for gcp
+    fi
+    bits=$(ip -o -f inet addr show $eth | awk '{print $4}')
+    IFS='/' read -ra netmask <<< "$bits"
+
+    if [ -n "$gateways" ]; then
+      gateway=$${gateways[$i]}
+      net="$net $net_option_name$enp/$subnet_inet/$${netmask[1]}/$gateway"
+    else
+      net="$net $net_option_name$eth" #aws
+    fi
+	done
+}
+
+# https://gist.github.com/fungusakafungus/1026804
+function retry {
+  local retry_max=$1
+  local retry_sleep=$2
+  shift 2
+  local count=$retry_max
+  while [ $count -gt 0 ]; do
+      "$@" && break
+      count=$(($count - 1))
+      echo "Retrying $* in $retry_sleep seconds..."
+      sleep $retry_sleep
+  done
+  [ $count -eq 0 ] && {
+      echo "Retry failed [$retry_max]: $*"
+      return 1
+  }
+  return 0
+}
