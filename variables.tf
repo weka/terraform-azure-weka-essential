d9c0c4a4af2082512b937233dd2ac80638b46be8
Assaf Giladi <assaf@weka.io>
2024-01-08 01:53:31 -0700

    chore: update default weka version 4.2.7.64

--- variables.tf
+++ variables.tf
@@ -80,7 +80,7 @@ variable "source_image_id" {
 variable "weka_version" {
   type        = string
   description = "The Weka version to deploy."
-  default     = "4.2.7"
+  default     = "4.2.7.64"
 }
 
 variable "get_weka_io_token" {
